e11a3b964ee6093bb7e67ef75a7895c008d17d3f
Syed-Rafi-Naqvi <sd.rafi1819@gmail.com>
2019-03-02 17:02:58 +0530

    path module imbibe

--- practise.js
+++ practise.js
@@ -1,8 +1,23 @@
-console.log('first')
+const path = require('path')
 
-setTimeout(()=> {
-    console.log('middle')
-}, 2000)
+let jn = path.join('rafi','naqvi')
+console.log(jn)
+
+let ab = path.isAbsolute(__filename)
+console.log(ab)
+
+let form = path.format({
+    root: '/',
+    dir: '/rafi/naqvi',
+    base: 'syed.txt'
+})
+console.log(form)
+
+let extention = path.extname('rafi.txt')
+console.log(extention)
+
+let dir = path.dirname(__filename)
+console.log(dir)
 
 
-console.log('last')
+console.log(path.parse(__filename))
