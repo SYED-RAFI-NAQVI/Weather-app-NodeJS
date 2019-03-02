4149083ba267ce0bc80797f80be504a7cf6aa9ef
Syed-Rafi-Naqvi <sd.rafi1819@gmail.com>
2019-03-02 20:02:40 +0530

    file system imbibe

--- practise.js
+++ practise.js
@@ -3,6 +3,8 @@
 //////////////////
 const path = require('path')
 
+console.log('Path Module')
+
 let jn = path.join('rafi','naqvi')
 console.log(`joining paths : ${jn}`)
 
@@ -25,3 +27,54 @@ console.log(`directory : ${dir}`)
 console.log('parse of filename :')
 console.log(path.parse(__filename))
 
+
+console.log('---------------------------------')
+////////////////////
+//////// OS ////////
+////////////////////
+
+const os = require('os')
+
+console.log('OS Module')
+
+let arch = os.arch()
+console.log(`architecture : ${arch}`)
+
+let cpu = os.cpus()
+console.log('cpu info :')
+console.log(cpu)
+
+let free = os.freemem()
+console.log(`Free Space : ${free}`)
+
+let host = os.hostname()
+console.log(`Host Name: ${host}`)
+
+let plat = os.platform()
+console.log(`platform : ${plat}`)
+
+let total = os.totalmem()
+console.log(`total memory : ${total}`)
+
+let upTime = os.uptime()
+console.log(`uptime : ${upTime}`)
+
+
+console.log('----------------------------')
+
+
+////////////////////////////////
+///////// File System //////////
+////////////////////////////////
+console.log('File System')
+
+const fs = require('fs')
+
+console.log('SYNC ---')
+console.log('files : ',fs.readdirSync('./'))
+
+console.log('ASYNC ---')
+fs.readdir('./', (error, files) => {
+    if (error) console.log('error', error)
+    else console.log('files : ' , files)
+})
