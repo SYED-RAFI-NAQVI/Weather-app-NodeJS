41788c27ab885f63de020ea0092ef870fc45bebf
Syed-Rafi-Naqvi <sd.rafi1819@gmail.com>
2019-03-14 02:22:21 +0530

    post method is added to app.js

--- practise/api/app.js
+++ practise/api/app.js
@@ -1,7 +1,11 @@
+const Joi = require('joi')
 const express = require('express')
 const app = express()
 
-const courses = [
+app.use(express.json())
+
+
+let courses = [
     {id : 1, name : 'course1'},
     {id : 2, name : 'course2'},
     {id : 3, name : 'course3'},
@@ -21,6 +25,26 @@ app.get('/api/courses/:id', (req, res) => {
     res.send(course)
 })
 
+app.post('/api/courses', (req, res) => {
+    const schema  =  {
+        name : Joi.string().min(3).required()
+    }
+
+    const result = Joi.validate(req.body, schema)
+
+
+    if (result.error){
+        res.status(400).send(result.error.message)
+        return
+    }
+
+    const course = {
+        id : courses.length + 1,
+        name : req.body.name
+    }
+    courses.push(course)
+    res.send(course)
+})
 
 const port = process.env.PORT || 2000
 
